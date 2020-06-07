6a0caf3e66452c6880ecdecd73997418c8bf0518
Администратор <evg2020>
2020-06-07 15:14:19 +0600

    3.4. Заголовок в файле java

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args){
-        System.out.println("19.01.20200");
+        System.out.println("19.01.2020");
     }
 
 }
