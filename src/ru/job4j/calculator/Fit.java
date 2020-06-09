b35e6d6d815313e1f1e68793e171658c67346d95
Администратор <evg2020>
2020-06-10 04:25:26 +0600

    6. Идеальный вес [#299150]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height){
+        double res = (height - 100) * 1.15;
+        return res;
+    }
+    public static double womanWeight(short height) {
+        double res = (height - 110) * 1.15;
+        return res;
+    }
+
+        public static void main (String[]args){
+            short height = 187;
+            double waight = Fit.manWeight(height);
+            System.out.println("Ваш вес не должен превышать " + waight + " кг");
+        }
+    }
+
+
+
