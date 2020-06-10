e3d015cf9d74182eb6897f88e884f59df0e557a3
Администратор <evg2020>
2020-06-10 17:03:39 +0600

    7. Расстояние между точками в системе координат [#299163]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,13 +2,11 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height){
-        double res = (height - 100) * 1.15;
-        return res;
-    }
+        return  (height - 100) * 1.15;
+            }
     public static double womanWeight(short height) {
-        double res = (height - 110) * 1.15;
-        return res;
-    }
+        return (height - 110) * 1.15;
+            }
         public static void main (String[]args){
             short height = 187;
             double waight = Fit.manWeight(height);
