982331f028300cd46c6979b3749ca3b340e6b3e2
Администратор <evg2020>
2020-06-10 13:58:19 +0600

    7. Расстояние между точками в системе координат [#299163]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -12,8 +12,12 @@ public class Fit {
 
         public static void main (String[]args){
             short height = 187;
+            int a,b,c;
+            a = 2;
+            c = 2;
+
             double waight = Fit.manWeight(height);
-            System.out.println("Для мужиков не должен превышать " + waight + " кг" );
+            System.out.println("Для мужиков не должен превышать " + waight + " кг"  );
             double waight1 = Fit.womanWeight(height);
             System.out.println("Для женщин шне должен превышать " + waight1 + " кг");
         }
---
+++ src/ru/job4j/condition/Point.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance( int x1, int x2, int y1, int y2){
+        double res = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+        return res;
+    }
+    public static void main (String[] args) {
+         int x1, x2, y1, y2;
+        x1 = 0;
+        y1 = 0;
+        x2 = 0;
+        y2 = 2;
+
+        double result = Point.distance(x1, x2, y1, y2);
+        System.out.println("distance (0, 0) to (2, 0) " + result);
+    }
+
+
+}
