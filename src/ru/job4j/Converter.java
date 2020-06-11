207c3c679426326e5ee5807f2ea81ce21b5b8389
Администратор <evg2020>
2020-06-11 12:28:15 +0600

    9.1. String [#299160]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -2,20 +2,23 @@ package ru.job4j;
 
 public class Converter {
    public static float rubleToEuro(float value) {
-       float rst = value / 70;
-       return rst;
+             return  value / 70;
    }
 
     public static float rubleToDollar(float value) {
-        float rst = value / 60;
-        return rst;
+                return value / 60;
     }
+
         public static void main(String[] args) {
-       float value =140;
+       float value = 140;
             float dollar = Converter.rubleToDollar(value);
             float euro = Converter.rubleToEuro(value);
-            System.out.println("Rub " + value+"----"+"Dollars " + dollar);
-            System.out.println("Rub " + value+"----"+"Euro " + euro);
+            float expetedEuro = value / 70;
+            float expetedDollar = value / 60;
+            boolean passedEuro = expetedEuro == euro;
+            boolean passedDollar = expetedDollar == dollar;
+            System.out.println("Rub " + value + "----" + "Dollars " + dollar + " ; " + passedDollar);
+            System.out.println("Rub " + value + "----" + "Euro " + euro + " ; " + passedEuro);
 
         }
 
