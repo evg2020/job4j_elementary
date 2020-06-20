19c5dfb6005ff9c009547024b144777344f864f8
Администратор <evg2020>
2020-06-21 01:00:29 +0400

    4.4. Максимум из трех чисел [#299119]

--- test/ru/job4j/condition/MultiMaxTest.java
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -7,9 +7,19 @@ import static org.junit.Assert.assertThat;
 public class MultiMaxTest {
 
     @Test
-    public void maxFrom3Number() {
-        int res = MultiMax.multiMax(1, 3, 2);
-        assertThat(res, is(3));
+    public void max1From3Number() {
+        int res = MultiMax.multiMax(11, 3, 2);
+        assertThat(res, is(11));
+
+    }
+    public void max2From3Number() {
+        int res = MultiMax.multiMax(1, 11, 2);
+        assertThat(res, is(11));
+
+    }
+    public void equalFrom3Number() {
+        int res = MultiMax.multiMax(11, 11, 11);
+        assertThat(res, is(11));
 
     }
 }
