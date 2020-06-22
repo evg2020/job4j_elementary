937375e9fa9dc8977ff224d7c9995da3a8d8292d
Администратор <evg_study@list.ru>
2020-06-23 01:07:16 +0400

    5.3. Построить шахматную доску в псевдографике. [#299215]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik){
+        int month = 0;
+        while(ivan < nik) {
+            ivan *= 3;
+            nik *=2;
+
+            month += 1;
+        }
+        System.out.println("Nikolay " + nik);
+        System.out.println("Ivan " + ivan);
+        return  month;
+    }
+    public static void main(String[] args) {
+        int res = calc(90, 95);
+        System.out.println("Иван обгонит николая в тяге , мес = " +res);
+    }
+}
+
