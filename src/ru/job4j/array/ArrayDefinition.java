e42a4f2989aa22706deb56e055cabcf63984bab7
Администратор <evg_study@list.ru>
2020-06-24 20:00:58 +0400

    6.0.2. Заполнение массива. [#299138]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        int[] nam = new int[10];
+        boolean[] army = new boolean[100];
 
+        names[0] = "Penr Arsentev";
+        names[1] = " Evgenii Fedorenko";
+        names[2] = "Richard Pipe";
+        names[3] = "Viktor Pelevin";
     }
 }
