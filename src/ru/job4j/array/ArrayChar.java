a3e7b93444a79dcb772aa5dda875587f3c105b58
Администратор <evg_study@list.ru>
2020-06-26 16:36:05 +0300

    6.4. Слово начинается с ... [#299126]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+//Метод принимает два массива символов. В цикле for вам нужно сравнить значения ячеек с одинаковым индексом. Если все ячейки из массива pref соответствуют таким же ячейкам в массиве word, значит, нужно вернуть true.
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                    result = false;
+                    break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        System.out.println(result);
+    }
+}
--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -1,5 +1,5 @@
 package ru.job4j.array;
-
+//6.3. Массив заполнен true или false
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
