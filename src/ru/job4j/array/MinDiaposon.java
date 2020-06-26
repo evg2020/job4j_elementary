d187df6df870c9927450ede8676092eb9ef4ee07
Администратор <evg_study@list.ru>
2020-06-26 22:02:56 +0300

    6.5.2. Поиск минимума в диапазоне. [#299134]

--- src/ru/job4j/array/MinDiaposon.java
+++ src/ru/job4j/array/MinDiaposon.java
@@ -1,11 +1,11 @@
 package ru.job4j.array;
 
 public class MinDiaposon {
-    public static int findMin(int[] data, int start, int finish) {
-        int min = data[start];
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
         for (int i = start; i <= finish; i++) {
-            if (min > data[i]) {
-                min =  data[i];
+            if (min > array[i]) {
+                min =  array[i];
             }
         }
 
