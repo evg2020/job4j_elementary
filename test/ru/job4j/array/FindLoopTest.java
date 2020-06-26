8327664eae484f70291a690564fc59881f3986d9
Администратор <evg_study@list.ru>
2020-06-26 09:37:55 +0300

    6.1. Классический поиск перебором. [#299124]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -14,4 +14,14 @@ public class FindLoopTest {
         int expect = 4;
         assertThat(res, is(expect));
     }
+
+    @Test
+    public void wheArrayHas6Thennothing() {
+        int value = 5;
+        int[] input = new int[]{1, 2, 3, 4, 5};
+        int res = FindLoop.indexOf(input, value);
+        int expect = 6;
+        assertThat(res, is(expect));
+    }
+
 }
