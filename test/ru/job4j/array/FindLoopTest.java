e8830c60029870449c78980f0611a16f988d508a
Администратор <evg_study@list.ru>
2020-06-26 09:32:04 +0300

    6.1. Классический поиск перебором. [#299124]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int value = 5;
+        int[] input = new int[]{1, 2, 3, 4, 5};
+        int res = FindLoop.indexOf(input, value);
+        int expect = 4;
+        assertThat(res, is(expect));
+    }
+}
