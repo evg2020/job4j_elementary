91f277ea0f5d29d6dfc58a5442126492a0ecec95
Администратор <evg_study@list.ru>
2020-06-26 22:02:05 +0300

    6.5.1. Поиск минимального числа в массиве. [#299133]

---
+++ src/ru/job4j/array/MinDiaposon.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class MinDiaposon {
+    public static int findMin(int[] data, int start, int finish) {
+        int min = data[start];
+        for (int i = start; i <= finish; i++) {
+            if (min > data[i]) {
+                min =  data[i];
+            }
+        }
+
+        return min;
+    }
+}
---
+++ test/ru/job4j/array/MinDiaposonTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class MinDiaposonTest {
+
+    @Test
+    public void whenFirstMin() {
+        assertThat(
+                MinDiaposon.findMin(
+                        new int[] {-1, 0, 5, 10},
+                        1, 3
+                ),
+                is(0)
+        );
+    }
+
+    @Test
+    public void whenLastMin() {
+        assertThat(
+                MinDiaposon.findMin(
+                        new int[] {10, 5, 3, 1},
+                        1, 3
+                ),
+                is(1)
+        );
+    }
+
+    @Test
+    public void whenMiddleMin() {
+        assertThat(
+                MinDiaposon.findMin(
+                        new int[] {10, 2, 5, 1},
+                        0, 2
+                ),
+                is(2)
+        );
+    }
+}
