e86a7add3939fdc04175f26993588dfc4a2187e7
Администратор <evg_study@list.ru>
2020-06-28 18:06:30 +0300

    6.7.4. Выигрышные комбинации в сокобан [#299128]

--- src/ru/job4j/array/Defragment2.java
+++ src/ru/job4j/array/Defragment2.java
@@ -1,6 +1,20 @@
 package ru.job4j.array;
-
+//од, который будет перемещать заполненные ячейки массива в начало, а пустые в конец.
+// метод заполнения
 public class Defragment2 {
+    public static String[] compress(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            String rowNull = rowNull(array);
+            int indexNull = indexNull(array);
+                String temp = array[i];
+                array[i] = array[indexNull];
+                array[indexNull] = temp;
+
+        }
+        return array;
+    }
+
+
     public static String rowNull(String[] array) {
         String rowNull = array[1];
         for (int i = 0; i < array.length; i++) {
@@ -22,4 +36,13 @@ public class Defragment2 {
         }
         return res;
     }
+
+    public static void main(String[] args) {
+        String[] array = {"I", null, "wanna", null, "be", null, "compressed", null};
+        String[] res = compress(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+
 }
